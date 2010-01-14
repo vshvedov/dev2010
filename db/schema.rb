07903fecedc460e1f8dc6d659d0bf09b1845af9b
Alex Lopez <webodessa@gmail.com>
2010-01-15 00:22:55 +0200

    http on form fieds

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100113201517) do
+ActiveRecord::Schema.define(:version => 20100113022228) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(:version => 20100113201517) do
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
     t.integer  "visits",            :default => 0
-    t.text     "interview"
   end
 
 end
